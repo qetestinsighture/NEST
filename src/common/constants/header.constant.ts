7576f3761fc4a73a536f277b5f7110c244b31528
Thameera Fernando <ThameeraSithnuwan>
2025-03-26 20:20:57 +0530

    Removed package***json and test yarn***lock***
    Updated imports in espondentController***ts and generateSampleJobs***ts to use the centralized mongoose export @redem-gmbh/redem-3***-db-schemas***

--- src/common/constants/header.constant.ts
+++ src/common/constants/header.constant.ts
@@ -2,4 +2,5 @@ export const TRACE_ID = 'x-trace-id';
 export const ORGANIZATION_ID = 'x-organization-id';
 export const PROJECT_ID = 'x-project-id';
 export const CLUSTER_ID = 'x-cluster-id';
+export const TEST_ID = 'x-cluster';
 export const HEADERS = { TRACE_ID, ORGANIZATION_ID, PROJECT_ID, CLUSTER_ID };
